e3b2db692a59083cc9ff209b17e90c3c4e4571ea
adistroe <97851663+adistroe@users.noreply.github.com>
2023-04-29 13:31:54 +0300

    Delete Pawns-Only Chess/task/src/chess directory

--- Pawns-Only Chess/task/src/chess/Color.kt
+++
@@ -1,7 +0,0 @@
-package chess
-
-//  standard chess colors
-enum class Color(val value: String) {
-    BLACK("Black"),
-    WHITE("White")
-}
--- Pawns-Only Chess/task/src/chess/Player.kt
+++
@@ -1,6 +0,0 @@
-package chess
-
-//  white player moves first
-data class Player(val name: String, val color: Color) {
-    var lastMove: String = ""
-}
